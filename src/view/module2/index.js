518e446f450f81e6d473d9e4fd43dc77d2a7ca8b
songkai <2511026858@qq.com>
2020-08-26 18:03:20 +0800

    使用lodash

--- src/view/module2/index.js
+++ src/view/module2/index.js
@@ -1,2 +1,15 @@
 import $ from '../../js/jquery.min.js';
-console.log('module2')
+import _ from 'lodash';
+
+console.log($);
+console.log(_);
+
+let createEle = () =>{
+    const element = document.createElement('div');
+    element.innerHTML = _.join(['Hello','webpack'], ' ');
+    document.body.appendChild(element);
+}
+
+createEle();
+
+
